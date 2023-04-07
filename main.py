bcce0fa54457151ac7f1d2fb437db3e8f5ccef8f
Saif Mahmud <67528956+mridul-1999@users.noreply.github.com>
2023-04-07 14:09:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Day 6 Challenge
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+
+user_name = input("User Name > ")
+password = input("Password >")
+
+if user_name == "David" and password == "password":
+  print("why hello there ",user_name,", what a lovely accent you have, you could have charsed you way in there are even without a password.")
+elif user_name == "suzanne" and password == "su74een":
+  print("Hi",user_name,"You are looking so cool.")
+elif user_name == "suzanne" and password == "su74een":
+  print("Hey",user_name,"what's Up man! ")
+else:
+  print("you do not have access. go Away")
+
+print()
+print("Have a great day \n")
+print("Don't forget to wear a hat in the sun!")
+
+'''
+# fix my code 
+
+season = input("what is your favorite season? ")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp.Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
+  '''
